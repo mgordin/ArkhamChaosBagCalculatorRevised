12376f5f9cfe4bc048d77c76948028bb0f213b5d
Mikhail Gordin <mikhail.gordin@gmail.com>
2024-03-16 11:44:30 -0700

    Moved out of weird subfolder

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ArkhamChaosBagCalculatorRevised/',
   plugins: [
     Vue({
       template: { transformAssetUrls }
